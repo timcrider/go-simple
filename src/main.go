9dffdc58a12df0eb12a89ea797da33ee73126689
Timothy Crider <timothy.crider@sas.com>
2018-11-15 08:23:13 -0500

    Cleanup

--- src/main.go
+++ src/main.go
@@ -9,7 +9,6 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("ping: %s\n", r.URL.Path)
 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-
 	})
 
 	http.ListenAndServe(":8088", nil)
