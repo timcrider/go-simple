289ff43668b04988b23a2d15f8c6e8fac931ffae
Tim Crider <timcrider@Karazhan.local>
2019-07-31 07:44:36 -0400

    Adding getting started app

---
+++ getting-started/build-local.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+MYDIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+cd $MYDIR
+
+GOOS=""
+
+if [[ "$1" != "all" ]] ; then
+	if [[ "$OSTYPE" == "linux-gnu" ]]; then
+		GOOS="linux"
+	elif [[ "$OSTYPE" == "darwin"* ]]; then
+		GOOS="darwin"
+	fi
+fi
+
+if [[ "${GOOS}" != "" ]] ; then
+	echo "Building specific binary: ${GOOS}"
+	CMD="go build -v -o ${MYDIR}/bin/simpleweb-${GOOS}"
+	echo "Running: $CMD"
+	eval "$CMD"
+else
+	# Build Linux binary
+	GOOS="linux"
+	echo "Building specific binary: ${GOOS}"
+	CMD="go build -v -o ${MYDIR}/bin/simpleweb-${GOOS}"
+	echo "Running: $CMD"
+	eval "$CMD"
+
+	# Build MacOSX binary
+	GOOS="darwin"
+	echo "Building specific binary: ${GOOS}"
+	CMD="go build -v -o ${MYDIR}/bin/simpleweb-${GOOS}"
+	echo "Running: $CMD"
+	eval "$CMD"
+
+	# Build Windows binary
+	GOOS="windows"
+	echo "Building specific binary: ${GOOS}"
+	CMD="go build -v -o ${MYDIR}/bin/simpleweb.exe"
+	echo "Running: $CMD"
+	eval "$CMD"
+
+fi
+
+
---
+++ getting-started/go.mod
@@ -0,0 +1,3 @@
+module github.com/timcrider/go-simple/getting-started
+
+go 1.12
